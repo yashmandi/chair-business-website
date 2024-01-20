a70d62fb1b0d893c8e47d1851268264f2c628243
yashmandi <yashmandi18@gmail.com>
2024-01-20 23:00:14 +0530

    updated

--- src/main/java/com/chair/App.java
+++ src/main/java/com/chair/App.java
@@ -1,13 +1,13 @@
 package com.chair;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Starting Spring Boot Application...");
+        SpringApplication.run(App.class, args);
+        System.out.println("Spring Boot Application has Started...");
     }
 }
